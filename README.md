2fe7c9a70b3b1c76b9eb2136a534bb25ec535779
Scott W Harden <swharden@gmail.com>
2020-02-01 22:20:38 -0500

    create Xamarin Forms version

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # NernstApp
-NernstApp is an Android application which uses the [Nernst equation](https://en.wikipedia.org/wiki/Nernst_equation) to calculate the equilibrium potential (Nernst potential) for an ion based on its charge and concentration on each side of a selectively-permanent membrane. This tool can be used to estimate the reversal potential of an ion across a cell membrane which can be useful in the fields of neuroscience, electrophysiology, and cellular neurophysiology.
+NernstApp is a mobile app which uses the [Nernst equation](https://en.wikipedia.org/wiki/Nernst_equation) to calculate the equilibrium potential (Nernst potential) for an ion based on its charge and concentration on each side of a selectively-permanent membrane. This tool can be used to estimate the reversal potential of an ion across the cell membrane of a neuron.
 
-![](dev/screenshot.jpg)
+![](src/Xamarin.Forms/screenshot.jpg)
 
-![](dev/screenshot2.jpg)
+![](src/Xamarin.Native.Android/screenshot2.jpg)
+
+## Developer Notes
+
+NernstApp is an open source project created in Visual Studio Community 2019. Aspiring mobile developers may find the source code and project structure useful to review. Code provided here demonstrates how to make the same application in both native Xamarin and Xamarin.Forms.
+
+**NernstApp 1.5** is a Xamarin Forms project which supports Android and iPhone. Source code for the GUI ([MainPage.xaml](/src/Xamarin.Forms/NernstApp/MainPage.xaml)) and logic ([MainPage.xaml.cs](/src/Xamarin.Forms/NernstApp/MainPage.xaml.cs)) is easy to review.
+
+**NernstApp 1.4** is a Xamarin native project which only supports Android. Source code for the GUI ([content_main.xml](/src/Xamarin.Native.Android/Resources/layout/content_main.xml) and logic ([MainActivity.cs](/src/Xamarin.Native.Android/MainActivity.cs)) is easy to review.
---
+++ src/Xamarin.Forms/NernstApp/MainPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NernstApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateNernstPotential();
+
+            // make clicking the title go to the website
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                Xamarin.Essentials.Browser.OpenAsync(
+                    uri: "http://www.GitHub.com/SWHarden/NernstApp", 
+                    Xamarin.Essentials.BrowserLaunchMode.SystemPreferred
+                    );
+            };
+            labelTitle.GestureRecognizers.Add(tapGestureRecognizer);
+            labelSubtitle.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+
+        private double CalculateEK(double concOut, double concIn, double charge, double temperatureKelvin = 300)
+        {
+            const double R = 8.314; // universal gas constant (Joules per Kelvin per mole)
+            const double F = 96485; // Faraday's constant (Coulombs per mole)
+            try
+            {
+                double Vek = (R * temperatureKelvin) / (charge * F) * Math.Log(concOut / concIn);
+                return Vek;
+            }
+            catch
+            {
+                return double.NaN;
+            }
+        }
+
+        private void UpdateNernstPotential()
+        {
+            // update temperature
+            try
+            {
+                double tempK = double.Parse(entryTemperatureK.Text);
+                double tempC = tempK - 273.15;
+                double tempF = tempK * 9.0 / 5.0 - 459.67;
+                labelTemperatureC.Text = Math.Round(tempC, 2).ToString();
+                labelTemperatureF.Text = Math.Round(tempF, 2).ToString();
+            }
+            catch
+            {
+                labelTemperatureC.Text = "invalid input";
+                labelTemperatureF.Text = "invalid input";
+            }
+
+            // update Nernst potential
+            try
+            {
+                double Vek = CalculateEK(
+                    concOut: double.Parse(entryExternal.Text),
+                    concIn: double.Parse(entryInternal.Text),
+                    charge: double.Parse(entryCharge.Text),
+                    temperatureKelvin: double.Parse(entryTemperatureK.Text)
+                    );
+                labelNernstPotential.Text = string.Format("{0:0.00} mV", Vek * 1000);
+            }
+            catch
+            {
+                labelNernstPotential.Text = "invalid input";
+            }
+        }
+
+        private void entryInternal_Completed(object sender, EventArgs e)
+        {
+            UpdateNernstPotential();
+        }
+
+        private void entryExternal_Completed(object sender, EventArgs e)
+        {
+            UpdateNernstPotential();
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            UpdateNernstPotential();
+        }
+
+        private void entryCharge_Completed(object sender, EventArgs e)
+        {
+            UpdateNernstPotential();
+        }
+
+        private void entryTemperatureK_Completed(object sender, EventArgs e)
+        {
+            UpdateNernstPotential();
+        }
+
+        private void buttonCl_Pressed(object sender, EventArgs e)
+        {
+            entryCharge.Text = "-1";
+        }
+
+        private void buttonNa_Pressed(object sender, EventArgs e)
+        {
+            entryCharge.Text = "1";
+        }
+
+        private void buttonK_Pressed(object sender, EventArgs e)
+        {
+            entryCharge.Text = "1";
+        }
+
+        private void buttonCa_Pressed(object sender, EventArgs e)
+        {
+            entryCharge.Text = "2";
+        }
+    }
+}
